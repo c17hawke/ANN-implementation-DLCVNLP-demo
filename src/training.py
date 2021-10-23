805c63e76c21af272be16d17c3a0a09d5f793698
c17hawke <2016peb5126@mnit.ac.in>
2021-10-23 17:26:25 +0530

    callback implimentation started

--- src/training.py
+++ src/training.py
@@ -2,6 +2,7 @@ import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model
+from src.utils.callbacks import get_callbacks
 import argparse
 
 def training(config_path):
@@ -20,8 +21,10 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION_SET = (X_valid, y_valid)
 
+    CALLBACK_LIST = get_callbacks(config, X_train)
+
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                    validation_data=VALIDATION_SET, callbacks=CALLBACK_LIST)
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
---
+++ src/utils/callbacks.py
@@ -0,0 +1,5 @@
+
+
+
+def get_callbacks(config, X_train):
+    pass
