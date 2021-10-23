6b1330953055c5c21a595d3fceeba7a8825ee455
c17hawke <2016peb5126@mnit.ac.in>
2021-10-23 17:25:44 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.0.2",
     author="c17hawke",
     description="A small package for ANN Implementation",
     long_description=long_description,
